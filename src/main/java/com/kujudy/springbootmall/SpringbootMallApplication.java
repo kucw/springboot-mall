84274f897f44204074b617364f43ed9b779f6892
kucw <pk74323jacky@gmail.com>
2022-03-08 05:03:59 +0800

    初始化 Spring Boot project

---
+++ src/main/java/com/kujudy/springbootmall/SpringbootMallApplication.java
@@ -0,0 +1,13 @@
+package com.kujudy.springbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kujudy/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kujudy.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
